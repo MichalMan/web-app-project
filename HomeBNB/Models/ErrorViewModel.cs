50315f379730f36145b1b42b67d2f3d346a75393
michalman <m.mich92@gmail.com>
2019-09-11 23:23:05 +0300

    init project

---
+++ HomeBNB/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HomeBNB.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
